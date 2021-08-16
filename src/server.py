51888365015fd1ea7fe320e1a4b4bde43b54e5f7
thelimpbacon <61944046+thelimpbacon@users.noreply.github.com>
2021-08-16 14:34:58 +0200

    add logger and save logs to csv

--- src/server.py
+++ src/server.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 from flask import Flask, request
 from flask_socketio import SocketIO
-import eventlet
 from threading import Lock
 from sensors import Switch
+import csv
+import time
 
 sensorPin1 = 25
 sensorPin2 = 20
@@ -41,21 +42,46 @@ class SocketServer:
 
             sensor_data = self.sensorBlock.read_value()
 
+            # specific data
+            sensor1 = sensor_data["sensor1"]
+            sensor2 = sensor_data["sensor2"]
+            sensor3 = sensor_data["sensor3"]
+            sensor4 = sensor_data["sensor4"]
+            sensor5 = sensor_data["sensor5"]
+            sensor6 = sensor_data["sensor6"]
+            sensor7 = sensor_data["sensor7"]
+            sensor8 = sensor_data["sensor8"]
+
+            # log sensor data to disk
+            self.writer.writerow([sensor1, sensor2, sensor3, sensor4,
+                                  sensor5, sensor6, sensor7, sensor8, tick, time.time()])
+
             self.socketio.emit("sensors", {
-                               "sensor1": sensor_data["sensor1"], "sensor2": sensor_data["sensor2"], "sensor3": sensor_data["sensor3"], "sensor4": sensor_data["sensor4"], "sensor5": sensor_data["sensor5"], "sensor6": sensor_data["sensor6"],  "sensor7": sensor_data["sensor7"], "sensor8": sensor_data["sensor8"], "tick": tick})
+                "sensor1": sensor1, "sensor2": sensor2, "sensor3": sensor3, "sensor4": sensor4,
+                "sensor5": sensor5, "sensor6": sensor6,  "sensor7": sensor7, "sensor8": sensor8, "tick": tick})
             tick += 1
 
             if tick == 500:
                 tick = 0
 
     def connect(self):
+        # open csv and creat css writer
+        log_path = "./logs/" + request.sid + ".csv"
+        self.file = open(log_path, "w")
+        self.writer = csv.writer(self.file)
+        self.writer.writerow(["sensor1", "sensor2", "sensor3", "sensor4",
+                             "sensor5", "sensor6", "sensor7", "sensor8", "tick", "actual_time"])
+
         with self.thread_lock:
             if self.thread is None:
                 self.thread = self.socketio.start_background_task(
                     self.background_thread)
+
         print("Connected", request.sid)
 
     def disconnect(self):
+        # close csv
+        self.file.close()
         print("Disconnected", request.sid)
 
     def run(self):
@@ -64,6 +90,6 @@ class SocketServer:
 
 
 if __name__ == '__main__':
-    server = SocketServer(sensorBlock=Switch(
+    server = SocketServer(host="10.3.141.1", sensorBlock=Switch(
         sensorPin1, sensorPin2, sensorPin3, sensorPin4, sensorPin5, sensorPin6, sensorPin7, sensorPin8))
     server.run()
